bee9c447ce03456f9bd1228c4a9b8d0fb21c94fc
Matias Donoso <rmttck@gmail.com>
2022-05-16 03:19:29 -0400

    make it better

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -11,9 +11,8 @@ const checkAuth = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = await User.findById(decoded.id).select(
-        "-password -confirmated -token -createdAt -updatedAt"
-      );
+      req.user = await User.findById(decoded.id);
+      console.log(req.user);
 
       return next();
     } catch (error) {
